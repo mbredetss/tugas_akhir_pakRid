5d1fe6533f594ef89dd4643dfe4bb5874ef15669
mighdad <mr.mighdadjaba@gmail.com>
2024-12-14 07:34:01 +0800

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/notification_provider.dart';
-import 'screens/class_screen.dart';
+import 'screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
